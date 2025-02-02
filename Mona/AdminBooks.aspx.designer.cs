952ec4a9bd69334dee110500f8a7b2c29b324efd
Mona-Tamimi <munatamimi65@gmail.com>
2025-02-02 17:29:33 +0300

    MONA

--- Mona/AdminBooks.aspx.designer.cs
+++ Mona/AdminBooks.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace Group5.Mona
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Image2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image2;
+
+        /// <summary>
         /// form2 control.
         /// </summary>
         /// <remarks>
@@ -139,5 +148,14 @@ namespace Group5.Mona
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvRequestedBooks;
+
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
