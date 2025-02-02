9f83938e5d6b96f855c4aeccb81a45f733d92c4f
sa-GT <habeeb.alafeef6@gmail.com>
2025-02-02 15:51:40 +0300

    able

--- Habib/Habib1.aspx.cs
+++ Habib/Habib1.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,5 +15,77 @@ namespace Group5.Habib
         {
 
         }
-    }
+		protected void ChangeView(object sender, EventArgs e)
+		{
+			// Get the CommandArgument from the clicked LinkButton
+			var linkButton = (System.Web.UI.WebControls.LinkButton)sender;
+			string viewName = linkButton.CommandArgument;
+
+			// Debugging: Print the viewName to the output
+			System.Diagnostics.Debug.WriteLine("Switching to view: " + viewName);
+
+			// Switch to the corresponding view
+			switch (viewName)
+			{
+				case "Dashboard":
+					multiView.ActiveViewIndex = 0;
+					break;
+				case "Books":
+					multiView.ActiveViewIndex = 1;
+					break;
+				case "Rooms":
+					multiView.ActiveViewIndex = 2;
+					break;
+				// Ignore "Feedback," "Team," and "Logout" cases
+				case "Feedback":
+				case "Team":
+				case "Logout":
+					// Do nothing
+					break;
+				default:
+					multiView.ActiveViewIndex = 0; // Default to Dashboard
+					break;
+			}
+		}
+
+		protected void btnAddNewBook_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnEdit1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnDelete1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnEdit2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnDelete2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		protected void lnkDashboard_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void lnkLogout_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("Home.aspx");
+		}
+	}
 }
