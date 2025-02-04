3227b94b5085555998dd32a616591b1f1940efb1
sajedakhasawneh <sajedakhasawneh5@gmail.com>
2025-02-04 08:33:59 +0300

    btnEdit

--- Sajeda/resetPassword.aspx.designer.cs
+++ Sajeda/resetPassword.aspx.designer.cs
@@ -51,13 +51,13 @@ namespace Group5
         protected global::System.Web.UI.WebControls.Button Button5;
 
         /// <summary>
-        /// resetPassword control.
+        /// setNewPassword control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button resetPassword;
+        protected global::System.Web.UI.WebControls.Button setNewPassword;
 
         /// <summary>
         /// editimg control.
